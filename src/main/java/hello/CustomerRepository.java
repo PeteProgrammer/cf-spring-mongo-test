f89e8bd3856bfbf10a270ae762b5a579a8207a84
Peter Strøiman <peter@stroiman.com>
2017-01-24 10:35:04 +0000

    Skeleton solution

--- src/main/java/hello/CustomerRepository.java
+++ src/main/java/hello/CustomerRepository.java
@@ -1,8 +1,13 @@
 package hello;
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
 public interface CustomerRepository extends MongoRepository<Customer, String> {
-    public Customer findByFirstName(String firstName);
-    public List<Customer> findByLastName(String lastName);
+    public List<Customer> findByFirstName(@Param("name") String name);
+    public List<Customer> findByLastName(@Param("name") String name);
 }
